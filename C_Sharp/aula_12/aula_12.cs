6edde1ddfd22193b2e38228154ce2d7e0af4e745
Tiago Raposo Lobo <tiagolobo2010@gmail.com>
2024-01-28 17:50:11 +0000

    aula_12

---
+++ C_Sharp/aula_12/aula_12.cs
@@ -0,0 +1,8 @@
+﻿var names = new List<string> { "Tiago", "Scott", "David", "Lucas" };
+names.Add("Rafael");
+names.Remove("Lucas");
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name}!");
+}
+// O "foreach" serve para repetir um bloco de código para cada elemento de uma lista. new List<t> serve para criar uma nova lista. O "Add" serve para adicionar um elemento na lista. O "Remove" serve para remover um elemento da lista.
