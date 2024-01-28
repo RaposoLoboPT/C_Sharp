fe660dde5b32b0058c39fba3e74fb4a2007e8e3f
Tiago Raposo Lobo <tiagolobo2010@gmail.com>
2024-01-28 16:45:19 +0000

    aula_10

---
+++ C_Sharp/aula_10/aula_10.cs
@@ -0,0 +1,15 @@
+﻿int counter = 0;
+while (counter < 10)
+{
+    Console.WriteLine(counter);
+    counter++;
+}
+// O "while" serve para repetir um bloco de código enquanto uma condição for verdadeira.
+counter = 0;
+do
+{
+    Console.WriteLine(counter);
+    counter++;
+}
+while (counter < 5);
+// O "do while" serve para repetir um bloco de código pelo menos uma vez, enquanto uma condição for verdadeira.
--- C_Sharp/aula_9/aula_9.cs
+++ C_Sharp/aula_9/aula_9.cs
@@ -3,7 +3,7 @@ int a = int.Parse(Console.ReadLine());
 // O Console.ReadLine() serve para pedir uma entrada do usário. O int.Parse() serve para transformar uma string em um inteiro, mas também podemos usar o double, o bool entre outros.
 Console.Write("Enter another number: ");
 int b = int.Parse(Console.ReadLine());
-if ((a + b) >= 10) 
+if ((a + b) > 10) 
 {
     Console.WriteLine("The answer is greather than 10.");
 } // O "if" serve para dizer se algo é verdadeiro.
@@ -13,7 +13,7 @@ else
 } // O "else" serve para dizer se algo é falso.
 Console.Write("Enter a third number: ");
 int c = int.Parse(Console.ReadLine());
-if ((a + b + c) >= 15 && (a == b)) // O "&&" serve para dizer se as duas condicões são verdadeiras. O "||" serve para dizer se pelo menos uma das condicões é verdadeira. O "!" serve para inverter o valor da condição. O "==" serve para dizer se o valor da esquerda e do valor da direita são iguais.
+if ((a + b + c) > 15 && (a == b)) // O "&&" serve para dizer se as duas condicões são verdadeiras. O "||" serve para dizer se pelo menos uma das condicões é verdadeira. O "!" serve para inverter o valor da condição. O "==" serve para dizer se o valor da esquerda e do valor da direita são iguais.
 {
     Console.WriteLine("The answer is greather than 15.");
     Console.WriteLine("And the first number is equal to the second.");
