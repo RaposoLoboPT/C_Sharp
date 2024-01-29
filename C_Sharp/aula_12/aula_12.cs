3c9a2f0f053e632b79f4c845c567583654ccabf7
Tiago Raposo Lobo <tiagolobo2010@gmail.com>
2024-01-29 21:50:32 +0000

    aula_13

--- C_Sharp/aula_12/aula_12.cs
+++ C_Sharp/aula_12/aula_12.cs
@@ -1,8 +1,15 @@
 ﻿var names = new List<string> { "Tiago", "Scott", "David", "Lucas" };
 names.Add("Rafael");
 names.Remove("Lucas");
+// new List<t> serve para criar uma nova lista. O "Add" serve para adicionar um elemento na lista. O "Remove" serve para remover um elemento da lista.
 foreach (var name in names)
 {
     Console.WriteLine($"Hello {name}!");
 }
-// O "foreach" serve para repetir um bloco de código para cada elemento de uma lista. new List<t> serve para criar uma nova lista. O "Add" serve para adicionar um elemento na lista. O "Remove" serve para remover um elemento da lista.
+// O "foreach" serve para repetir um bloco de código para cada elemento de uma lista.
+Console.WriteLine(names[0]);
+// O "names[0]" serve para mostrar o elemento da lista na posição 0 (1º).
+Console.WriteLine(names[names.Count - 1]);
+// O "names.Count" serve para mostrar o número de elementos da lista.
+Console.WriteLine(names[^3]);
+// O "name[^3]" serve para mostrar se um elemento está na lista a contar do final (seria o scott, porque ainda temos o lucas).
---
+++ C_Sharp/aula_13/aula_13.cs
@@ -0,0 +1,8 @@
+﻿var names = new string[] { "Tiago", "Scott", "David", "Rafael"};
+// O "new string[]" é um array de strings, que serve para criar uma estrutura de dados para armazenar elementos do mesmo tipo. Os arrays são fixos.
+names = [..names,"Anyone"];
+// É assim que se adiciona um elemento ao array.
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name}!");
+}
