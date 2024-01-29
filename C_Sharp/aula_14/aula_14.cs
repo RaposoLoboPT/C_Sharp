d5f64917f7528e34c174ec3c358d10dbb5244d9a
Tiago Raposo Lobo <tiagolobo2010@gmail.com>
2024-01-29 22:30:22 +0000

    aula_14

---
+++ C_Sharp/aula_14/aula_14.cs
@@ -0,0 +1,9 @@
+﻿var numbers = new List<int> { 14, 38, 56, 40, 29, 71,65};
+numbers.Sort();
+// O "Sort" serve para ordenar uma lista de elementos.
+Console.WriteLine($"I found 29 at index:{numbers.IndexOf(29)}");
+// O "IndexOf" serve para mostrar o indice de um elemento na lista.
+foreach (var number in numbers)
+{
+    Console.WriteLine($"Look at {number}!");
+}
