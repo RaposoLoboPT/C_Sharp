6130c9e23f615fd550c58d1ad0c3eef3184cf063
Tiago Raposo Lobo <tiagolobo2010@gmail.com>
2024-01-30 14:23:04 +0000

    aula_15

---
+++ C_Sharp/aula_15/aula_15.cs
@@ -0,0 +1,19 @@
+﻿int[] scores = [97, 92, 81, 60];
+
+IEnumerable<int> scoreQuery = 
+    from score in scores
+    where score > 80
+    select score;
+foreach (int i in scoreQuery)
+{
+    Console.WriteLine(i + " ");
+}
+int l = 0;
+while (l < scores.Length)
+{
+    if(scores[l] > 90)
+    {
+        Console.WriteLine($"Found a score greater than 90: {scores[l]}");
+    }
+    l++;
+}
