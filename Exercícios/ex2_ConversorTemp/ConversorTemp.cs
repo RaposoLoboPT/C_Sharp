8d94bb9855027a78f6db09a351c11aff8a6defe7
Tiago Raposo Lobo <tiagolobo2010@gmail.com>
2024-02-14 22:51:26 +0000

    ex2_CoversorDeTemperatura

---
+++ Exercícios/ex2_ConversorTemp/ConversorTemp.cs
@@ -0,0 +1,19 @@
+﻿// Vamos criar um convertor de temperatura de celsius para fahrenheit e vice-versa.
+Console.WriteLine("Diga para que tipo de temperatura deseja converter (Celsius ou Fahrenheit): ");
+var TempType = Console.ReadLine();
+Console.WriteLine("Diga a temperatura que deseja converter: ");
+var TempValue = double.Parse(Console.ReadLine());
+if (TempType.ToLower() == "celsius")
+{
+    double celsius = (TempValue - 32) / 1.8;
+    Console.WriteLine($"A temperatura em celsius é: {celsius}");
+}
+else if (TempType.ToLower() == "fahrenheit")
+{
+    double fahrenheit = TempValue * 1.8 + 32;
+    Console.WriteLine($"A temperatura em fahrenhit é: {fahrenheit}");
+}
+else
+{
+    Console.WriteLine("[ERROR] Temperatura inválida");
+}
